ec6db47ed3e86bc74a5f9dfa7dea75abac677e66
somekoder <somekoder@gmail.com>
2025-02-19 14:19:38 -0500

    Tested CreateUserUseCase

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,12 @@ repositories {
 }
 
 dependencies {
+
+    // Testing
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
+    testImplementation(libs.mockk)
+
     implementation(libs.ktor.server.rate.limiting)
     implementation(libs.koin.ktor)
     implementation(libs.koin.logger.slf4j)
@@ -36,8 +42,7 @@ dependencies {
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.config.yaml)
-    testImplementation(libs.ktor.server.test.host)
-    testImplementation(libs.kotlin.test.junit)
+
 
     // Exposed
     implementation(libs.exposed.core)
